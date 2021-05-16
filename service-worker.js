b1e18c11d54ca3daa7eb88e87c83883ee278e020
Tinh Van Le <tinh-van.le@ap.omron.net>
2021-05-16 22:19:04 +0700

    move nhanh

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9aeb044085626fda9fd05814a5632672.js"
+  "/precache-manifest.6f814a103540b032629f2390ce6c6987.js"
 );
 
 self.addEventListener('message', (event) => {
